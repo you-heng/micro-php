af373a347b168ccf8460717d5062d823aff81eef
有衡 <43592453+you-heng@users.noreply.github.com>
2022-09-16 18:22:21 +0800

    update

--- app/index/Index.php
+++ app/index/Index.php
@@ -1,9 +1,13 @@
 <?php
 namespace app\index;
 
+use lib\Config;
+
 class index{
     public function index()
     {
-        echo "index-index-index--多应用模式";
+        $app = Config::get('app.module');
+        dump($app);
+        //echo json_encode(['code' => 0, 'msg' => '多应用模式']);
     }
 }
